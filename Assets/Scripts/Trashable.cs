1f51e178ee5088ee9086633ece708b3406fb08ed
Ian Fisher <loktar@gmail.com>
2019-01-26 10:04:34 -0800

    Added Ian's dev scene and Trashable script

---
+++ Assets/Scripts/Trashable.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trashable : MonoBehaviour
+{
+    private bool isTrashing;
+
+    public float trashTime = 5f;
+    public float percentTrashed = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (isTrashing)
+        {
+            percentTrashed += Time.fixedDeltaTime / trashTime;
+            CheckIfFullyTrashed();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        isTrashing = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isTrashing = false;
+    }
+
+    private void CheckIfFullyTrashed()
+    {
+        if (percentTrashed >= 1f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
