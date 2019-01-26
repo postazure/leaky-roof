aab79a3b6a1e87da2f36496469df688a79987fe2
Ian Fisher <loktar@gmail.com>
2019-01-26 12:45:06 -0800

    Added leaks that spawn water drops in dev scene.

---
+++ Assets/Scripts/Leak.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leak : MonoBehaviour
+{
+    public bool active = true;
+    public GameObject waterDrop;
+
+    public float initialDripDelay = 0;
+
+    [Range(0.01f, int.MaxValue)]
+    public float secondsBetweenDrips = 1;
+
+    private float lastDripTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var delay = initialDripDelay + UnityEngine.Random.Range(0, 1f);
+        lastDripTime = Time.fixedTime + delay;
+    }
+
+    void FixedUpdate()
+    {
+        float now = Time.fixedTime;
+        bool shouldDrip = now - lastDripTime > secondsBetweenDrips;
+
+        if (shouldDrip)
+        {
+            SpawnDrip();
+            lastDripTime = now;
+        }
+    }
+
+    private void SpawnDrip()
+    {
+        var newDrop = Instantiate<GameObject>(waterDrop, this.transform.position, Quaternion.identity);
+    }
+}
