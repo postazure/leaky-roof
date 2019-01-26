0b21a008a720712bce0de1cb46c5f62b8448599d
Max Gordon <gordonmaxc@gmail.com>
2019-01-26 11:06:19 -0800

    select with nose

---
+++ Assets/Scripts/PlayerSelectorController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelectorController : MonoBehaviour
+{
+    private GameObject selectedObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (selectedObject)
+            {
+                print("Engage was pressed with a selection");
+            }
+            else
+            {
+                print("Engage was pressed without a selection");
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        selectedObject = other.gameObject;    
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        selectedObject = null;
+    }
+}
