0cc86eebe8933c39cb855f7dcbb172dcef4aab6a
Max Gordon <gordonmaxc@gmail.com>
2019-01-27 13:40:02 -0800

    add instruction scene

---
+++ Assets/Scripts/AutoSceneAdvance.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AutoSceneAdvance : MonoBehaviour
+{
+
+    public int delayInSeconds;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.fixedTime > delayInSeconds)
+        {
+            SceneController.instance.LoadNextScene();
+        }
+    }
+}
