ef1238582f897bec64b43e273573387868651c38
Francis Li <mail@francisli.com>
2019-01-26 20:20:53 -0800

    Refactor scripts for Box and Garbage, add support for Degradable showing soggy mesh

--- Assets/Scripts/Degradeable.cs
+++ Assets/Scripts/Degradeable.cs
@@ -11,30 +11,64 @@ public class Degradeable : MonoBehaviour
     public float degradePerSecond = 0.1f;
 
     public bool isDegrading;
+    public bool isDegraded;
 
     public Color degradedColor;
 
     private Color initialColor;
 
+    Renderer GetRenderer()
+    {
+        //// for prefab objects like Box, which have multiple different
+        //// meshes as children for the different states, there will not be 
+        //// a top-level renderer
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer == null)
+        {
+            renderer = GetComponentInChildren<Renderer>();
+        }
+        return renderer;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        initialColor = GetComponent<Renderer>().material.color;
+        initialColor = GetRenderer().material.color;
     }
 
     // Update is called once per frame
     void Update()
     {
         var currentColor = Color.Lerp(initialColor, degradedColor, percentDegraded);
-        GetComponent<Renderer>().material.color = currentColor;
+        GetRenderer().material.color = currentColor;
     }
 
     private void FixedUpdate()
     {
-        if (isDegrading)
+        if (isDegrading && !isDegraded)
         {
             percentDegraded += Time.fixedDeltaTime * degradePerSecond;
+            if (percentDegraded >= 1f)
+            {
+                isDegraded = true;
+
+                //// change to "soggy" state if exists
+                Transform soggy = gameObject.transform.Find("soggy");
+                if (soggy != null)
+                {
+                    //// deactivate all the other states
+                    for (int i = 0; i < gameObject.transform.childCount; i++)
+                    {
+                        Transform transform = gameObject.transform.GetChild(i);
+                        if (!transform.Equals(soggy))
+                        {
+                            transform.gameObject.SetActive(false);
+                        }
+                    }
+                    //// set the soggy state active
+                    soggy.gameObject.SetActive(true);
+                }
+            }
         }
     }
 
