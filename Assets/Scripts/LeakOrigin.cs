bd528a16a3c420953d02a7df5cc2b90b9a222fd1
Max Gordon <gordonmaxc@gmail.com>
2019-01-26 19:39:57 -0800

    create leak generator

---
+++ Assets/Scripts/LeakOrigin.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeakOrigin : MonoBehaviour
+{
+    public void ActivateLeak(GameObject leakPrefab, Vector3 location)
+    {
+        Instantiate(leakPrefab, location, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/LeakOriginController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeakOriginController : MonoBehaviour
+{
+    public GameObject leakPrefab;
+    public List<GameObject> leakOriginLocations;
+    public float intervaulDurationSeconds;
+
+    private float lastLeakStartTime = 0f;
+    private GameObject leak;
+
+    void Update()
+    {
+        if(Time.fixedTime - lastLeakStartTime > intervaulDurationSeconds && leakOriginLocations.Count > 0)
+        {
+            StartNextLeak();
+        }
+    }
+
+    void StartNextLeak()
+    {
+        lastLeakStartTime = Time.fixedTime;
+
+
+        int r = Random.Range(0, leakOriginLocations.Count);
+        GameObject location = leakOriginLocations[r];
+        leakOriginLocations.RemoveAt(r);
+
+        LeakOrigin leakLocation = location.GetComponent<LeakOrigin>();
+        leakLocation.ActivateLeak(leakPrefab, location.transform.position);
+    }
+}
