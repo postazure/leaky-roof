b52946638d59d7368940604391d3b7e05b6980bd
Ian Fisher <loktar@gmail.com>
2019-01-26 17:59:27 -0800

    Puddle groups spawn randomly placed puddles

--- Assets/Scripts/Puddle.cs
+++ Assets/Scripts/Puddle.cs
@@ -8,10 +8,16 @@ public class Puddle : MonoBehaviour
     public float percentPerDrop = 0.1f;
 
     private float percentageFull;
+    private Vector3 initialScale;
+
+    private void Start()
+    {
+        initialScale = transform.localScale;
+    }
 
     private void Update()
     {
-        transform.localScale = new Vector3(percentageFull, transform.localScale.y, percentageFull);
+        transform.localScale = new Vector3(initialScale.x * percentageFull, initialScale.y, initialScale.z * percentageFull);
     }
 
     internal void Increment()
--- Assets/Scripts/PuddleGroup.cs
+++ Assets/Scripts/PuddleGroup.cs
@@ -1,24 +1,54 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class PuddleGroup : MonoBehaviour
 {
     public Puddle puddlePrefab;
+    public float maxPuddleSplash = 0.25f;
 
     private List<Puddle> puddles = new List<Puddle>();
 
     public void Increment()
     {
-        Puddle puddle = puddles.Find(p => !p.IsFull());
-        if (!puddle)
+        Puddle puddle;
+
+        if (puddles.Count == 0)
         {
-            puddle = Instantiate(puddlePrefab);
-            puddle.transform.parent = gameObject.transform;
-            puddle.transform.localPosition = Vector3.zero;
-            puddles.Add(puddle);
+            puddle = AddPuddle(Vector3.zero);
+        }
+        else
+        {
+            puddle = IncrementOrAddPuddle();
         }
 
         puddle.Increment();
+    }
+
+    private Puddle IncrementOrAddPuddle()
+    {
+        Puddle puddle = puddles.Find(p => !p.IsFull());
+        if (!puddle)
+        {
+            Vector3 position = new Vector3(RandomPuddleSplash(), 0, RandomPuddleSplash());
+            puddle = AddPuddle(position);
+        }
+
+        return puddle;
+    }
+
+    private float RandomPuddleSplash()
+    {
+        return Random.Range(-maxPuddleSplash, maxPuddleSplash);
+    }
+
+    private Puddle AddPuddle(Vector3 position)
+    {
+        position.y = FindObjectsOfType<Puddle>().Length * 0.0001f; // Hax: prevent weird color highlight when puddles overlap
+
+        Puddle puddle = Instantiate(puddlePrefab);
+        puddle.transform.parent = gameObject.transform;
+        puddle.transform.localPosition = position;
+        puddles.Add(puddle);
+        return puddle;
     }
 }
