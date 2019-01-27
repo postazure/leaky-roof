df488ae7bbcc5fbc876e10e31d6d673a346bb2cf
David <davidbecerra@users.noreply.github.com>
2019-01-26 20:58:08 -0800

    Add title scene

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+
+    private static SceneController _instance;
+    public static SceneController instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        // Singleton
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+    }
+
+    public void LoadNextScene()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene + 1);
+    }
+}
---
+++ Assets/Scripts/StartGameButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameButton : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneController.instance.LoadNextScene();
+        }
+    }
+}
